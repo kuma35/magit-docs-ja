bd611bc722e38117260bad05858f9158b2540b6f
kuma35 <sindobook@gmail.com>
2021-08-19 04:14:13 +0900

    delete locale folder.
    JP:localeフォルダを削除。
    
    po4aはtexinfoファイル(*.texi)をいじってしまい、訳文生成後に修正が必要。
    そのためpo4aを使わず直接 texinfoファイル(*.texi)を翻訳することにする。

--- Documentation/locale/first_gettext.sh
+++
@@ -1,2 +0,0 @@
-po4a-gettextize --format texinfo --master ../magit.texi --master-charset utf-8 --copyright-holder "magit" --package-name "magit" --package-version "0.0" --po ./ja/LC_MESSAGES/magit.texi.po
-po4a-gettextize --format texinfo --master ../magit-section.texi --master-charset utf-8 --copyright-holder "magit" --package-name "magit" --package-version "0.0" --po ./ja/LC_MESSAGES/magit-section.texi.po
--- Documentation/locale/mk_gettext_script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find .. -type d -name locale -prune -o -type f -name "*.texi" -printf "po4a-gettextize --format texinfo --master %p --master-charset utf-8 --copyright-holder \"magit\" --package-name \"magit\" --package-version \"0.0\" --po ./ja/LC_MESSAGES/%f.po\n"
